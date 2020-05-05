dbd86fe5750384ab4ab45bc9761554e00b1116e6
astatsuya <sharekobekko@gmail.com>
2020-05-05 12:22:20 +0900

    add axios and json-server

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,3 +1,11 @@
+import { commonApi } from '@js/utils/API/config';
+import { endpoints } from '@js/utils/API/endpoints';
+
 export const testAlert = (value) => {
   alert(value);
+};
+
+export const getSampleRequest = async () => {
+  const { data } = await commonApi.get(endpoints.testArray);
+  alert(JSON.stringify(data));
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import { testAlert } from '@js/sample';
+import { getSampleRequest } from '@js/sample';
 import './style.scss';
 
 const testButton = document.getElementById('testButton');
 testButton.addEventListener('click', () => {
-  testAlert('test');
+  getSampleRequest();
 });
