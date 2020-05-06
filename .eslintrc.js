c064a4fb8ee54fbb563e5e9936bebad9d1adc0ec
astatsuya <sharekobekko@gmail.com>
2020-05-06 18:02:42 +0900

    modify eslint settings to watch config files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    process: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
